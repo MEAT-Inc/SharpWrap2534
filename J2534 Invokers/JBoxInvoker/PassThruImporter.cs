d70a08b9fdc847276358d0099f0efba4607ee698
Zack Walsh <zachary,walsh@opusivs.com>
2021-10-27 11:04:24 -0400

    Enum extensiosn and DLL Paths.
    Added static DLL Paths and included enum description extensions.

--- J2534 Invokers/JBoxInvoker/PassThruImporter.cs
+++ J2534 Invokers/JBoxInvoker/PassThruImporter.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Reflection;
 using System.Text;
@@ -18,7 +19,23 @@ namespace JBoxInvoker
         public string JDllPath;
         public Assembly JDllAssembly;
 
-        // -----------------------------------------------------------------------------
+        // Enum types for standard DLL values.
+        public enum PassThruPaths
+        {
+            // CDP3
+            [Description(@"C:\Program Files (x86)\Drew Technologies, Inc\J2534\CarDAQ Plus 3\cardaqplus3_0404_32.dll")]
+            CarDAQPlus3_0404 = 0x10,
+            [Description(@"C:\Program Files (x86)\Drew Technologies, Inc\J2534\CarDAQ Plus 3\0500\cardaqplus3_0500_32.dll")]
+            CarDAQPlus3_0500 = 0x11,
+
+            // CDP4
+            [Description(@"C:\Program Files (x86)\Drew Technologies, Inc\J2534\CarDAQ Plus 4\cardaqplus4_0404_32.dll")]
+            CarDAQPlus4_0404 = 0x21,           
+            [Description(@"C:\Program Files (x86)\Drew Technologies, Inc\J2534\CarDAQ Plus 4\0500\cardaqplus4_0500_32.dll")]
+            CarDAQPlus4_0500 = 0x22,
+        }
+
+        // ---------------------------------------------------------------------------------------------
 
         /// <summary>
         /// Imports a new JDLL into the project and stores all of its outputs.
@@ -26,7 +43,19 @@ namespace JBoxInvoker
         /// <param name="DllPath"></param>
         public PassThruImporter(string DllPath)
         {
-
+            // Store the DLL path ehre and import the path as an assy.
+            this.JDllPath = DllPath;
+            this.JDllAssembly = Assembly.LoadFile(this.JDllPath);
+        }
+        /// <summary>
+        /// Imports a new JDLL into the project and stores all of its outputs.
+        /// </summary>
+        /// <param name="DllType">Enum type fo the DLL to import.</param>
+        public PassThruImporter(PassThruPaths DllType)
+        {
+            // Store DLL path and import as an assy. 
+            this.JDllPath = DllType.ToDescriptionString();
+            this.JDllAssembly = Assembly.LoadFile(this.JDllPath);
         }
     }
 }

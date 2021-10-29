bbfd356d559ca20b34b9085a0f031ec4f92e6456
Zack Walsh <zachary.walsh@opusivs.com>
2021-10-29 14:55:10 -0400

    Added class instance for DLL Locating.
    DLL Locator type built.

---
+++ J2534 Invokers/JBoxInvoker/PassThruLogic/PassThruImport/PassThruImportDLLs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JBoxInvoker.PassThruLogic.PassThruImport
+{
+    /// <summary>
+    /// This class contains the logic needed to build and use new PassThru DLLs from the J2534 DLL object type.
+    /// </summary>
+    public class PassThruImportDLLs
+    {
+
+    }
+}
--- J2534 Invokers/JBoxInvoker/PassThruLogic/PassThruTypes/PassThruConstants.cs
+++ J2534 Invokers/JBoxInvoker/PassThruLogic/PassThruTypes/PassThruConstants.cs
@@ -12,6 +12,10 @@ namespace JBoxInvoker.PassThruLogic.PassThruTypes
     /// </summary>
     public class PassThruConstants
     {
+        // Registry Keys for PassThruSupport.
+        public static readonly string PASSTHRU_REGISTRY_PATH = "Software\\PassThruSupport.04.04";
+        public static readonly string PASSTHRU_REGISTRY_PATH_6432 = "Software\\Wow6432Node\\PassThruSupport.04.04";
+
         // JVersion.
         public JVersion Version { get; private set; }
 
@@ -23,7 +27,7 @@ namespace JBoxInvoker.PassThruLogic.PassThruTypes
         // --------------------------------------------------------------------------------------
 
         /// <summary>
-        /// Builds a new set of passthru constants based on the JVersion
+        /// Builds a new set of PassThru constants based on the JVersion
         /// </summary>
         /// <param name="J2524Version">Version of the API</param>
         public PassThruConstants(JVersion J2524Version) { this.Version = J2524Version; }

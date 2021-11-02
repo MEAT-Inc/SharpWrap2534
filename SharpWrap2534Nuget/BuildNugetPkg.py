6ab2ffe2af572049fe01a0abde9f1f9a1ca8c852
Zack Walsh <zachary.walsh@opusivs.com>
2021-11-02 16:32:39 -0400

    Autopush added to NugetPy script
    Python script now pushes to nuget automatically.

--- SharpWrap2534Nuget/BuildNugetPkg.py
+++ SharpWrap2534Nuget/BuildNugetPkg.py
@@ -5,26 +5,25 @@ import re
 
 # Store the args here and check if there's enough of them
 args_array = sys.argv
-if (len(args_array) < 4): 
+if (len(args_array) < 3): 
     print ("Arguments are NuSpecFile, Version, Tag Value, [Changes]") 
     exit(0)
 
 # Arg setup
-# Arg 0 - Path to nuspec
 # Arg 1 - Version Number
 # Arg 2 - Tag Value
 # Arg 3 - Change Notes
 
 # Store nuspec file and version value
-nuspec_file_name = args_array[1]
-version_value = "<version>" + args_array[2] + "</version>"
-version_tag_value = "<tags>" + args_array[3] + "</tags>"
-if (len(args_array) == 4): changelog_contents = ""
-else: changelog_contents = "<releaseNotes>" + args_array[4] + "</releaseNotes>"
+template_nuspec_file = ".\NupkgConfig\\_SharpWrap2534.nuspec.base"
+version_value = "<version>" + args_array[1] + "</version>"
+version_tag_value = "<tags>" + args_array[2] + "</tags>"
+if (len(args_array) == 3): changelog_contents = ""
+else: changelog_contents = "<releaseNotes>" + args_array[3] + "</releaseNotes>"
 
 # Read contents of the nuspec file.
-with open(nuspec_file_name, "r+") as nuspec_file:
-    nuspec_contents = nuspec_file.read()
+with open(template_nuspec_file, "r") as input_nuspec_file:
+    nuspec_contents = input_nuspec_file.read()
 
     # Regex the nuspec file to build output.
     new_nuspec_contents = re.sub("<version>((\d+|\.|\>)+)<\/version>", version_value, nuspec_contents)
@@ -35,16 +34,27 @@ with open(nuspec_file_name, "r+") as nuspec_file:
         new_nuspec_contents = re.sub("<releaseNotes>(([^<])+)<\/releaseNotes>", changelog_contents, new_nuspec_contents)
 
     # Close and delete file.
-    nuspec_file.close()
-
+    input_nuspec_file.close()
 
 # Write new values out and run the pack command.
-os.remove(nuspec_file_name)
-with open (nuspec_file_name, "w") as nuspec_file:
+output_spec_file = "SharpWrap2534_" + version_value + '.nuspec'
+with open (output_spec_file, "w") as nuspec_file:
     nuspec_file.write(new_nuspec_contents)
     nuspec_file.close()
 
 # Run the pack command now.
-nuget_command = 'nuget pack ' + nuspec_file_name + ' -OutputDirectory NupkgFiles'
-print ('Running nuget pack command: ' + nuget_command)
-os.popen(nuget_command)
+nuget_pack_command = 'nuget pack ' + output_spec_file + ' -OutputDirectory NupkgOutput'
+print ('Running nuget pack command: ' + nuget_pack_command)
+os.popen(nuget_pack_command)
+
+# Split output
+print ('-------------------------------------------------------------')
+
+# Push the package into the repo
+nuget_package_file = '.\NupkgOutput\SharpWrap2534.' + version_value + '.nupkg'
+nuget_push_command = 'nuget push ' + nuget_package_file + '-Source \"github\" -ConfigFile .\NupkgConfig\\_SharpWrap2534.nuget.config'
+print ('Running nuget push command: ' + nuget_push_command) 
+os.popen(nuget_push_command)
+
+# Split output
+print ('-------------------------------------------------------------')

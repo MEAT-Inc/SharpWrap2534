48690add44107d5fb5666fe6f126188912562282
Zack Walsh <zachary.walsh@opusivs.com>
2022-01-07 14:23:41 -0500

    Built code changes for the new UI project
    New project for UI controls and built instance for our installed DLLs view content

---
+++ SharpWrap2534-UI/SharpWrapViewModels/SharpWrapInstalledDLLsViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpWrap2534_UI.SharpWrapViewModels
+{
+    /// <summary>
+    /// View model for a list of SharpWrap DLL instances
+    /// </summary>
+    public class SharpWrapInstalledDLLsViewModel
+    {
+    }
+}
--- SharpWrap2534Nuget/BuildNugetPkg.py
+++ SharpWrap2534Nuget/BuildNugetPkg.py
@@ -6,8 +6,8 @@ import time
 
 # Store the args here and check if there's enough of them
 args_array = sys.argv
-if (len(args_array) < 3): 
-    print ("Arguments are NuSpecFile, Version, Tag Value, [Changes]") 
+if (len(args_array) < 4): 
+    print ("Arguments are App Name, NuSpecFile, Version, Tag Value, [Changes]") 
     exit(0)
 
 # Arg setup
@@ -16,30 +16,41 @@ if (len(args_array) < 3):
 # Arg 3 - Change Notes
 
 # Store nuspec file and version value
-template_nuspec_file = ".\\NupkgConfig\\_SharpWrap2534.nuspec.base"
-version_value = "<version>" + args_array[1] + "</version>"
-version_tag_value = "<tags>" + args_array[2] + "</tags>"
-if (len(args_array) == 3): changelog_contents = ""
-else: changelog_contents = "<releaseNotes>" + args_array[3] + "</releaseNotes>"
+application_package_name = args_array[1]
+template_nuspec_file = ".\\NupkgConfig\\_" + application_package_name + ".nuspec.base"
+version_value = "<version>" + args_array[2] + "</version>"
+version_tag_value = "<tags>" + args_array[3] + "</tags>"
+if (len(args_array) == 3): 
+    changelog_contents = ""
+    description_contents = ""
+else:
+    changelog_contents = "<releaseNotes>" + args_array[4] + "\n</releaseNotes>"
+    description_contents = args_array[4]
 
 # Read contents of the nuspec file.
 with open(template_nuspec_file, "r") as input_nuspec_file:
     nuspec_contents = input_nuspec_file.read()
 
     # Regex the nuspec file to build output.
-    new_nuspec_contents = re.sub("<version>((\d+|\.|\>)+)<\/version>", version_value, nuspec_contents)
-    new_nuspec_contents = re.sub("<tags>((\S)+)<\/tags>", version_tag_value, new_nuspec_contents)
+    new_nuspec_contents = re.sub(r"<version>((\d+|\.|\>)+)<\/version>", version_value, nuspec_contents)
+    new_nuspec_contents = re.sub(r"<tags>((\S)+)<\/tags>", version_tag_value, new_nuspec_contents)
 
     # Check if doing changelog.
     if (changelog_contents != ""):
-        new_nuspec_contents = re.sub("<releaseNotes>(([^<])+)<\/releaseNotes>", changelog_contents, new_nuspec_contents)
+        changelog_contents = re.sub(r"--\s+",  "\n- ", changelog_contents)
+        new_nuspec_contents = re.sub(r"<releaseNotes>(([^<])+)<\/releaseNotes>", changelog_contents.strip(), new_nuspec_contents)
 
+    # Check description update.
+    if (description_contents != ""):
+        description_contents = re.sub(r"--\s+", "\n- ", description_contents)
+        new_nuspec_contents = re.sub(r"    <\/description>", description_contents.strip() + "\n</description>", new_nuspec_contents)
+         
     # Close and delete file.
     input_nuspec_file.close()
 
 # Write new values out and run the pack command.
-output_spec_file = ".\\SharpWrap2534_" + args_array[1] + '.nuspec'
-history_spec_file = '.\\NupkgConfig\\SharpWrap2534_' + args_array[1] + '.nuspec'
+output_spec_file = ".\\" + application_package_name + "." + args_array[2] + ".nuspec"
+history_spec_file = ".\\NupkgConfig\\" + application_package_name + "." + args_array[2] + ".nuspec"
 if os.path.exists(output_spec_file): os.remove(output_spec_file) 
 if os.path.exists(history_spec_file): os.remove(history_spec_file) 
 
@@ -63,18 +74,18 @@ print ('--> Packed Package OK!')
 print ('-------------------------------------------------------------')
 
 # Push the package into the repo
-nuget_package_file = '.\\NupkgOutput\\SharpWrap2534.' + args_array[1] + '.nupkg'
+nuget_package_file = ".\\NupkgOutput\\" + application_package_name + "." + args_array[2] + '.nupkg'
 if os.path.exists(nuget_package_file): os.remove(nuget_package_file) 
 
 # Wait for settle.
 time.sleep(1.5)
-nuget_push_command = 'nuget push ' + nuget_package_file + ' -Source \"github\" -ConfigFile .\\NupkgConfig\\_SharpWrap2534.nuget.config -SkipDuplicate'
+nuget_push_command = 'nuget push ' + nuget_package_file + " -Source \"github\" -ConfigFile .\\NupkgConfig\\_" + application_package_name + ".nuget.config -SkipDuplicate"
 print ('Running nuget push now...')
 print ('--> Push Command: ' + nuget_push_command) 
 push_stream = os.popen(nuget_push_command)
 print ('--> Pushed to nuget OK!')
 
-# Remove old files.
+# # Remove old files.
 time.sleep(1.5)
 if os.path.exists(output_spec_file): os.remove(output_spec_file) 
 

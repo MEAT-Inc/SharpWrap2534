ea518b4302278a69f2f4068720bac32897b6c2af
Zack Walsh <zachary.walsh@opusivs.com>
2022-06-08 14:32:28 -0400

    Building for release

--- SharpWrapperExtensions/SharpSimulator/Properties/AssemblyInfo.cs
+++ SharpWrapperExtensions/SharpSimulator/Properties/AssemblyInfo.cs
@@ -19,7 +19,7 @@ using System.Resources;
 [assembly: Guid("183F7A6A-E107-4209-8A11-044A08DEAEE4")]
 
 // Version information
-[assembly: AssemblyVersion("0.5.3.63")]
-[assembly: AssemblyFileVersion("0.5.3.63")]
+[assembly: AssemblyVersion("0.5.4.64")]
+[assembly: AssemblyFileVersion("0.5.4.64")]
 [assembly: NeutralResourcesLanguageAttribute( "en-US" )]
 

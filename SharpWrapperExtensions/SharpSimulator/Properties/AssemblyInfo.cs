ff8ced087c3c1ceb68d8a085b44ff6f40c602146
Zack Walsh <zachary.walsh@opusivs.com>
2022-06-22 16:43:27 -0400

    Fixing baudrate issues

--- SharpWrapperExtensions/SharpSimulator/Properties/AssemblyInfo.cs
+++ SharpWrapperExtensions/SharpSimulator/Properties/AssemblyInfo.cs
@@ -19,7 +19,7 @@ using System.Resources;
 [assembly: Guid("183F7A6A-E107-4209-8A11-044A08DEAEE4")]
 
 // Version information
-[assembly: AssemblyVersion("0.7.10.103")]
-[assembly: AssemblyFileVersion("0.7.10.103")]
+[assembly: AssemblyVersion("0.7.11.104")]
+[assembly: AssemblyFileVersion("0.7.11.104")]
 [assembly: NeutralResourcesLanguageAttribute( "en-US" )]
 

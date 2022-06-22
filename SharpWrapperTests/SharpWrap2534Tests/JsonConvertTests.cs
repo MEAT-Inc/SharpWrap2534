e809f97225542dc8f2711dcd7b8c450203d6ed20
Zack Walsh <zachary.walsh@opusivs.com>
2022-06-22 14:26:00 -0400

    Unit test updates
    Updated unit tests

--- SharpWrapperTests/SharpWrap2534Tests/JsonConvertTests.cs
+++ SharpWrapperTests/SharpWrap2534Tests/JsonConvertTests.cs
@@ -4,7 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
+using SharpWrap2534.J2534Objects;
 using SharpWrap2534.PassThruTypes;
+using SharpWrap2534.SupportingLogic;
 
 namespace SharpWrap2534Tests
 {
@@ -21,14 +23,21 @@ namespace SharpWrap2534Tests
             // Convert tests execute here
             var ResultsList = new[]
             {
+                // Message Object Conversions
                 TestJsonMessageWrite(),    // Testing JSON convert from a message to string
-                TestJsonMessageRead()      // Testing JSON read into a message from a string
+                TestJsonMessageRead(),     // Testing JSON read into a message from a string
+
+                // Filter Object Conversions
+                TestJsonFilterWrite(),    // Testing JSON convert from filter to string
+                TestJsonFilterRead(),     // Testing JSON read into a filter from a string
             };
 
             // Return if all tests passed or not.
             return ResultsList.All(ResultObj => ResultObj);
         }
 
+        // ------------------------------------------------------------------------------------------------------------------------------------------
+        
         /// <summary>
         /// Tests conversion of messages out to JSON
         /// </summary>
@@ -44,10 +53,11 @@ namespace SharpWrap2534Tests
                 Data = new byte[] { 0x00, 0x00, 0x07, 0xE8, 0x41, 0x00, 0xBF, 0xFF, 0xB9, 0x93 },
             };
 
-            // Convert into a string value here
             try 
             {
+                // Convert into a string value here
                 string BuiltMsgString = JsonConvert.SerializeObject(MessageToConvert);
+                Console.WriteLine(BuiltMsgString);
                 return true;
             }
             catch { return false; }
@@ -62,8 +72,59 @@ namespace SharpWrap2534Tests
             string JsonToParse = "{\"ProtocolId\": \"ISO15765\", \"RxStatus\": \"No RxStatus\", \"TxFlags\": \"ISO15765_FRAME_PAD\", \"Timestamp\": \"10ms\", \"DataSize\": \"12 Bytes\", \"ExtraDataIndex\": 0, \"Data\": \"0x00 0x00 0x07 0xDF 0x02 0x09 0x02 0x00 0x00 0x00 0x00 0x00\"}";
 
             // Run the conversion
-            try {
-                PassThruStructs.PassThruMsg ConvertedFromJSON = JsonConvert.DeserializeObject<PassThruStructs.PassThruMsg>(JsonToParse);
+            try
+            {
+                _ = JsonConvert.DeserializeObject<PassThruStructs.PassThruMsg>(JsonToParse);
+                Console.WriteLine("CONVERTING FROM JSON TO MESSAGE PASSED!");
+                return true;
+            }
+            catch { return false; }
+        }
+
+        // ------------------------------------------------------------------------------------------------------------------------------------------
+
+        /// <summary>
+        /// Tests writing a JSON filter output from a given definition
+        /// </summary>
+        /// <returns>True if write passes. False if not.</returns>
+        private static bool TestJsonFilterWrite()
+        {
+            // Build a filter to convert out
+            var FilterToConvert = new J2534Filter()
+            {
+                FilterId = 0,
+                FilterFlags = 0x40,
+                FilterMask = "00 00 FF FF",
+                FilterPattern = "00 00 07 E0",
+                FilterFlowCtl = "00 00 07 E8",
+                FilterProtocol = ProtocolId.ISO15765,
+                FilterType = FilterDef.FLOW_CONTROL_FILTER,
+                FilterStatus = PTInstanceStatus.INITIALIZED,
+            };
+
+            try
+            {
+                // Convert into a string value here
+                string BuiltFilterString = JsonConvert.SerializeObject(FilterToConvert);
+                Console.WriteLine(BuiltFilterString);
+                return true;
+            }
+            catch { return false; }
+        }
+        /// <summary>
+        /// Tests reading a JSON filter output into a desired definition
+        /// </summary>
+        /// <returns>True if read passes. False if not.</returns>
+        private static bool TestJsonFilterRead()
+        {
+            // Setup our JSON string here
+            string JsonToParse = "{\"FilterFlags\":64,\"FilterType\":3,\"FilterProtocol\":6,\"FilterStatus\":1,\"FilterId\":0,\"FilterMask\":\"00 00 FF FF\",\"FilterPattern\":\"00 00 07 E0\",\"FilterFlowCtl\":\"00 00 07 E8\"}";
+
+            // Run the conversion
+            try
+            {
+                _ = JsonConvert.DeserializeObject<J2534Filter>(JsonToParse);
+                Console.WriteLine("CONVERTING FROM JSON TO FILTER OBJECT PASSED!");
                 return true;
             }
             catch { return false; }

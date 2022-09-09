e2166cb4b01072a9a9153bd8a840d5eb4bfcf698
Zack Walsh <zachary.walsh@opusivs.com>
2022-09-09 12:00:59 -0400

    More package updates

--- SharpAutoId/Properties/AssemblyInfo.cs
+++ SharpAutoId/Properties/AssemblyInfo.cs
@@ -19,7 +19,7 @@ using System.Resources;
 [assembly: Guid("73552094-035A-4DFB-BEC4-5116EC7993EF")]
 
 // Version information
-[assembly: AssemblyVersion("0.3.13.75")]
-[assembly: AssemblyFileVersion("0.3.13.75")]
+[assembly: AssemblyVersion("0.3.13.77")]
+[assembly: AssemblyFileVersion("0.3.13.77")]
 [assembly: NeutralResourcesLanguageAttribute( "en-US" )]
 
--- SharpSimulator/Properties/AssemblyInfo.cs
+++ SharpSimulator/Properties/AssemblyInfo.cs
@@ -19,7 +19,7 @@ using System.Resources;
 [assembly: Guid("183F7A6A-E107-4209-8A11-044A08DEAEE4")]
 
 // Version information
-[assembly: AssemblyVersion("0.8.1.145")]
-[assembly: AssemblyFileVersion("0.8.1.145")]
+[assembly: AssemblyVersion("0.8.1.146")]
+[assembly: AssemblyFileVersion("0.8.1.146")]
 [assembly: NeutralResourcesLanguageAttribute( "en-US" )]
 

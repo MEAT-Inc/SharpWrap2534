dbc60929f9e9d43c3a99ec1848447e3f6273646a
Zack Walsh <zachary.walsh@opusivs.com>
2023-02-03 07:19:48 -0500

    Testing projects for refactoring

--- Extensions/SharpPipes/Properties/AssemblyInfo.cs
+++ Extensions/SharpPipes/Properties/AssemblyInfo.cs
@@ -19,7 +19,7 @@ using System.Resources;
 [assembly: Guid("73552094-035A-4DFB-BEC4-5116EC7993EF")]
 
 // Version information
-[assembly: AssemblyVersion("0.1.3.17")]
-[assembly: AssemblyFileVersion("0.1.3.17")]
+[assembly: AssemblyVersion("0.1.3.18")]
+[assembly: AssemblyFileVersion("0.1.3.18")]
 [assembly: NeutralResourcesLanguageAttribute( "en-US" )]
 
--- SharpWrapper/Properties/AssemblyInfo.cs
+++ SharpWrapper/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Resources;
 [assembly: InternalsVisibleTo("SharpWrap2534Tests")]
 
 // Version information
-[assembly: AssemblyVersion("5.3.6.404")]
-[assembly: AssemblyFileVersion("5.3.6.404")]
+[assembly: AssemblyVersion("5.3.6.405")]
+[assembly: AssemblyFileVersion("5.3.6.405")]
 [assembly: NeutralResourcesLanguageAttribute( "en-US" )]
 

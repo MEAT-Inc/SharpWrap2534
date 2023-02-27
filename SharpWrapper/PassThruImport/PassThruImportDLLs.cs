e7c9f23fe37054d6256995519904d1eb342fc167
Zack Walsh <zack.walsh@meatinc.autos>
2023-02-27 14:20:55 -0500

    Built new FinalAllDlls Method
    Added new static method that can load and store all DLLs on the system from one call instead of forcing creation of a new method

--- SharpWrapper/PassThruImport/PassThruImportDLLs.cs
+++ SharpWrapper/PassThruImport/PassThruImportDLLs.cs
@@ -12,11 +12,16 @@ namespace SharpWrapper.PassThruImport
     /// </summary>
     public class PassThruImportDLLs
     {
+        #region Custom Events
+        #endregion //Custom Events
+
+        #region Fields
+
         // PT Support key values. Holds both 0404 locations and 0500 locations
         public readonly RegistryKey PassThruSupportKey_0404;
         public readonly RegistryKey PassThruSupportKey_0500;
         public readonly RegistryKey PassThruSupportKey_0404_6432;
-        public readonly RegistryKey PassThruSupportKey_0500_6432; 
+        public readonly RegistryKey PassThruSupportKey_0500_6432;
 
         // Key information and DLL Values for each of the different key locations found
         public readonly string[] DllKeyValues_0404;
@@ -27,7 +32,15 @@ namespace SharpWrapper.PassThruImport
         // List of all located DLL Values for both versions and store locations
         public readonly List<J2534Dll> LocatedJ2534DLLs;
 
-        // --------------------------------------------------------------------------------
+        #endregion //Fields
+
+        #region Properties
+        #endregion //Properties
+
+        #region Structs and Classes
+        #endregion //Structs and Classes
+
+        // ------------------------------------------------------------------------------------------------------------------------------------------
 
         /// <summary>
         /// Builds a new DLL importing object.
@@ -53,50 +66,19 @@ namespace SharpWrapper.PassThruImport
             LocatedJ2534DLLs.AddRange(this._getDLLsForKeyList(PassThruSupportKey_0404_6432, DllKeyValues_0404_6432));
             LocatedJ2534DLLs.AddRange(this._getDLLsForKeyList(PassThruSupportKey_0500_6432, DllKeyValues_0500_6432));
         }
+
+        // ------------------------------------------------------------------------------------------------------------------------------------------
+
         /// <summary>
-        /// Builds an array of new J2534 DLLs
+        /// Loads and returns all supported J2534 DLLs for our system
         /// </summary>
-        /// <param name="PassThruKey">DLL Parent Key</param>
-        /// <param name="DllKeys">DLL name</param>
-        /// <returns>A collection of built J2534 DLL instances for the given registry key and lookup locations</returns>
-        private J2534Dll[] _getDLLsForKeyList(RegistryKey PassThruKey, string[] DllKeys)
+        /// <returns>A Collection of built J2534 DLL instances</returns>
+        public static IEnumerable<J2534Dll> FindAllInstalledDLLs()
         {
-            // Build array set here.
-            var BuiltDLLs = DllKeys.Select(DllValue =>
-            {
-                // Build new DLL and get infos. Check our DLL Version first.
-                RegistryKey DeviceKey = PassThruKey.OpenSubKey(DllValue);
-                if (DeviceKey == null) return null;
-
-                // Find values here.
-                string VendorValue = (string)DeviceKey.GetValue("Vendor", "");
-                string ShortName = (string)DeviceKey.GetValue("Name", "");
-                string FunctionLibrary = (string)DeviceKey.GetValue("FunctionLibrary", "");
-
-                // Build a temporary list to hold our DLL protocols
-                List<ProtocolId> SupportedProtocols = new List<ProtocolId>();
-
-                // Look at all the supported protocols for this DLL
-                List<string> DeviceProtocols = DeviceKey.GetSubKeyNames().ToList();
-                foreach (var ProtocolKey in DeviceProtocols)
-                {
-                    // Check to see if we support this protocol or not
-                    if ((int)DeviceKey.GetValue(ProtocolKey, 0) == 0) continue;
-                    if (!Enum.TryParse(ProtocolKey, out ProtocolId SupportedProtocol)) continue;
-                    
-                    // If we found this protocol, then add it to our list of output protocols
-                    SupportedProtocols.Add(SupportedProtocol);
-                }
-
-                // Build and return the new DLL instance for this entry and exit out
-                return new J2534Dll(DllValue, VendorValue, ShortName, FunctionLibrary, SupportedProtocols);
-            }).Where(JDll => JDll != null).ToArray();
-
-            // Return built Values
-            return BuiltDLLs;
+            // Build a new DLL import instance and load in all the found DLLs
+            var DllImport = new PassThruImportDLLs();
+            return DllImport.LocatedJ2534DLLs;
         }
-
-        // ----------------------------------------------------------------------------------------------
 
         /// <summary>
         /// Finds the DLL provided based on the function lib name.
@@ -127,5 +109,49 @@ namespace SharpWrapper.PassThruImport
             // Return output based on DLL Value.
             return DllFound != null;
         }
+
+        /// <summary>
+        /// Builds an array of new J2534 DLLs
+        /// </summary>
+        /// <param name="PassThruKey">DLL Parent Key</param>
+        /// <param name="DllKeys">DLL name</param>
+        /// <returns>A collection of built J2534 DLL instances for the given registry key and lookup locations</returns>
+        private J2534Dll[] _getDLLsForKeyList(RegistryKey PassThruKey, string[] DllKeys)
+        {
+            // Build array set here.
+            var BuiltDLLs = DllKeys.Select(DllValue =>
+            {
+                // Build new DLL and get infos. Check our DLL Version first.
+                RegistryKey DeviceKey = PassThruKey.OpenSubKey(DllValue);
+                if (DeviceKey == null) return null;
+
+                // Find values here.
+                string VendorValue = (string)DeviceKey.GetValue("Vendor", "");
+                string ShortName = (string)DeviceKey.GetValue("Name", "");
+                string FunctionLibrary = (string)DeviceKey.GetValue("FunctionLibrary", "");
+
+                // Build a temporary list to hold our DLL protocols
+                List<ProtocolId> SupportedProtocols = new List<ProtocolId>();
+
+                // Look at all the supported protocols for this DLL
+                List<string> DeviceProtocols = DeviceKey.GetSubKeyNames().ToList();
+                foreach (var ProtocolKey in DeviceProtocols)
+                {
+                    // Check to see if we support this protocol or not
+                    if ((int)DeviceKey.GetValue(ProtocolKey, 0) == 0) continue;
+                    if (!Enum.TryParse(ProtocolKey, out ProtocolId SupportedProtocol)) continue;
+
+                    // If we found this protocol, then add it to our list of output protocols
+                    SupportedProtocols.Add(SupportedProtocol);
+                }
+
+                // Build and return the new DLL instance for this entry and exit out
+                return new J2534Dll(DllValue, VendorValue, ShortName, FunctionLibrary, SupportedProtocols);
+            }).Where(JDll => JDll != null).ToArray();
+
+            // Return built Values
+            return BuiltDLLs;
+        }
+
     }
 }

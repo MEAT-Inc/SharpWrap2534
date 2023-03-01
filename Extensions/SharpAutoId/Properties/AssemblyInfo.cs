9021c272cc5068c77bec239f741b9b8eebb03209
Zack Walsh <zack.walsh@meatinc.autos>
2023-03-01 09:57:34 -0500

    Added some new custom loggers for generators
    Generator objects now include custom targets for outputting content to dedicated log files.

--- Extensions/SharpAutoId/Properties/AssemblyInfo.cs
+++ Extensions/SharpAutoId/Properties/AssemblyInfo.cs
@@ -19,7 +19,7 @@ using System.Resources;
 [assembly: Guid("73552094-035A-4DFB-BEC4-5116EC7993EF")]
 
 // Version information
-[assembly: AssemblyVersion("0.4.3.115")]
-[assembly: AssemblyFileVersion("0.4.3.115")]
+[assembly: AssemblyVersion("0.4.3.117")]
+[assembly: AssemblyFileVersion("0.4.3.117")]
 [assembly: NeutralResourcesLanguageAttribute( "en-US" )]
 

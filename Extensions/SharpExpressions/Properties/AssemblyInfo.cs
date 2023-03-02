4d8254d69f2a2d30ae21a1d5bc63b6b08349531c
Zack Walsh <zack.walsh@meatinc.autos>
2023-03-02 12:04:01 -0500

    Updated version numbers and packages
    Updated some package versions and finally fixed logging issues with the generators

--- Extensions/SharpExpressions/Properties/AssemblyInfo.cs
+++ Extensions/SharpExpressions/Properties/AssemblyInfo.cs
@@ -19,7 +19,7 @@ using System.Resources;
 [assembly: Guid("73552094-035A-4DFB-BEC4-5116EC7993EF")]
 
 // Version information
-[assembly: AssemblyVersion("0.4.12.64")]
-[assembly: AssemblyFileVersion("0.4.12.64")]
+[assembly: AssemblyVersion("0.4.12.65")]
+[assembly: AssemblyFileVersion("0.4.12.65")]
 [assembly: NeutralResourcesLanguageAttribute( "en-US" )]
 
--- SharpWrapper/Properties/AssemblyInfo.cs
+++ SharpWrapper/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Resources;
 [assembly: InternalsVisibleTo("SharpWrap2534Tests")]
 
 // Version information
-[assembly: AssemblyVersion("6.1.5.440")]
-[assembly: AssemblyFileVersion("6.1.5.440")]
+[assembly: AssemblyVersion("6.1.5.441")]
+[assembly: AssemblyFileVersion("6.1.5.441")]
 [assembly: NeutralResourcesLanguageAttribute( "en-US" )]
 

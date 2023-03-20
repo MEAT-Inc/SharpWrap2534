17cf061a2bd0f2fa02b788f2ab726c1c5cf14580
Zack Walsh <zack.walsh@meatinc.autos>
2023-03-20 09:25:50 -0400

    Build and version release

--- SharpAutoId/Properties/AssemblyInfo.cs
+++ SharpAutoId/Properties/AssemblyInfo.cs
@@ -19,7 +19,7 @@ using System.Resources;
 [assembly: Guid("73552094-035A-4DFB-BEC4-5116EC7993EF")]
 
 // Version information
-[assembly: AssemblyVersion("0.4.3.126")]
-[assembly: AssemblyFileVersion("0.4.3.126")]
+[assembly: AssemblyVersion("0.4.3.127")]
+[assembly: AssemblyFileVersion("0.4.3.127")]
 [assembly: NeutralResourcesLanguageAttribute( "en-US" )]
 

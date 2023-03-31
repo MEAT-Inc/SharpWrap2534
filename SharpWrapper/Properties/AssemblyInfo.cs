77a7cf757ab5e327acd8811066702325a5a670f9
Zack Walsh <zachary.walsh@opusivs.com>
2023-03-31 09:41:13 -0400

    Upgraded internal package versions

--- SharpWrapper/Properties/AssemblyInfo.cs
+++ SharpWrapper/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Resources;
 [assembly: InternalsVisibleTo("SharpWrap2534Tests")]
 
 // Version information
-[assembly: AssemblyVersion("6.1.9.459")]
-[assembly: AssemblyFileVersion("6.1.9.459")]
+[assembly: AssemblyVersion("6.1.9.460")]
+[assembly: AssemblyFileVersion("6.1.9.460")]
 [assembly: NeutralResourcesLanguageAttribute( "en-US" )]
 

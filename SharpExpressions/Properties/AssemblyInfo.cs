774dedb293bd5af0396ddae58fd8225cbe91426d
Zack Walsh <zack.walsh@meatinc.autos>
2023-09-06 12:17:09 -0400

    Built new packages for all projects

--- SharpExpressions/Properties/AssemblyInfo.cs
+++ SharpExpressions/Properties/AssemblyInfo.cs
@@ -19,7 +19,7 @@ using System.Resources;
 [assembly: Guid("73552094-035A-4DFB-BEC4-5116EC7993EF")]
 
 // Version information
-[assembly: AssemblyVersion("0.5.12.215")]
-[assembly: AssemblyFileVersion("0.5.12.215")]
+[assembly: AssemblyVersion("0.5.12.216")]
+[assembly: AssemblyFileVersion("0.5.12.216")]
 [assembly: NeutralResourcesLanguageAttribute( "en-US" )]
 

507a589702944bea4ef0c17ab5ff245bc9031224
Zack Walsh <zack.walsh@meatinc.autos>
2023-09-26 12:23:13 -0400

    Updated simulation configurations

--- SharpExpressions/Properties/AssemblyInfo.cs
+++ SharpExpressions/Properties/AssemblyInfo.cs
@@ -19,7 +19,7 @@ using System.Resources;
 [assembly: Guid("73552094-035A-4DFB-BEC4-5116EC7993EF")]
 
 // Version information
-[assembly: AssemblyVersion("0.5.14.310")]
-[assembly: AssemblyFileVersion("0.5.14.310")]
+[assembly: AssemblyVersion("0.5.14.312")]
+[assembly: AssemblyFileVersion("0.5.14.312")]
 [assembly: NeutralResourcesLanguageAttribute( "en-US" )]
 

fdf384bf4880bba54ca2ee4a189da1c002928967
Zack Walsh <zack.walsh@meatinc.autos>
2023-09-28 13:47:32 -0400

    Changed logic for building responses
    Removed calls to write empty content if no values are found for responses.

--- SharpAutoId/Properties/AssemblyInfo.cs
+++ SharpAutoId/Properties/AssemblyInfo.cs
@@ -19,7 +19,7 @@ using System.Resources;
 [assembly: Guid("73552094-035A-4DFB-BEC4-5116EC7993EF")]
 
 // Version information
-[assembly: AssemblyVersion("0.4.4.153")]
-[assembly: AssemblyFileVersion("0.4.4.153")]
+[assembly: AssemblyVersion("0.4.4.156")]
+[assembly: AssemblyFileVersion("0.4.4.156")]
 [assembly: NeutralResourcesLanguageAttribute( "en-US" )]
 

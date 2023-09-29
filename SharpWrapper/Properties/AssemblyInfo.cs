bab164adba282d7e8456622a00ccdb911c11181d
Zack Walsh <zachary.walsh@opusivs.com>
2023-09-29 12:45:49 -0400

    Building new test packages

--- SharpWrapper/Properties/AssemblyInfo.cs
+++ SharpWrapper/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Resources;
 [assembly: InternalsVisibleTo("SharpWrap2534Tests")]
 
 // Version information
-[assembly: AssemblyVersion("6.2.3.508")]
-[assembly: AssemblyFileVersion("6.2.3.508")]
+[assembly: AssemblyVersion("6.2.3.509")]
+[assembly: AssemblyFileVersion("6.2.3.509")]
 [assembly: NeutralResourcesLanguageAttribute( "en-US" )]
 
